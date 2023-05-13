c9c8d087cdee7a18d8b23b1828847bb83b6cb8af
Juan Carlos Garcés Castro <126428321+JuanCarlosGarces@users.noreply.github.com>
2023-05-13 04:31:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+/* Declaramos nuestras variables de los formularios */ 
+const nombre = document.getElementsByClassName('.nombre');
+const telefono = document.getElementsByClassName('.telefono');
+const listadoDeContactos = document.getElementsByClassName('.listadoDeContactos');
+const baseDeDatos = window.localStorage
+
+/* Declaramos las variables de los botones */
+const btnAgregarContacto = document.getElementsByClassName('.agregar');
+const btnEditarContacto = document.getElementsByClassName('.editar');
+
+/*btnAgregarContacto.onclick = () => {
+	let contacto = {
+	id: Math.random(1, 100),
+	nombre: nombre.value,
+	telefono: telefono.value,
+	}
+	guardarContacto(baseDeDatos, contacto)
+}*/
+
+subirContactos(baseDeDatos);
+
